01a2982388b5253724009a09e4460876e65111d6
d-philip <djphilip@bu.edu>
2021-10-23 21:21:24 -0500

    Load image from local storage

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,29 +1,40 @@
 import * as React from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
 import Button from 'react-bootstrap/Button'
+import { ReactSVG } from 'react-svg'
 
 const Canvas = class extends React.Component {
     constructor(props) {
         super(props);
         this.saveImage = this.saveImage.bind(this);
-
         this.canvas = React.createRef();
+        this.state = {image: null};
     }
 
     saveImage = () => {
         this.canvas.current
             .exportSvg()
             .then(data => {
-                console.log(data);
+                // console.log(data);
+                localStorage.setItem('image', data);
             })
             .catch(e => {
                 console.log(e);
             });
     }
 
+    loadImage = () => {
+        const image = localStorage.getItem('image');
+        // var parser = new DOMParser();
+        // var parsedImage = parser.parseFromString(image, "image/svg+xml");
+        console.log(image);
+        this.setState({image: image});
+    }
+
     render() {
         return (
             <div>
+                <img src={`data:image/svg+xml;utf8,${encodeURIComponent(this.state.image)}`} />
                 <h2>{this.props.title}</h2>
                 <ReactSketchCanvas
                     ref={this.canvas}
@@ -31,7 +42,10 @@ const Canvas = class extends React.Component {
                     strokeColor="black"
                 />
                 <Button onClick={this.saveImage}>
-                    Get Image
+                    Save Image
+                </Button>
+                <Button onClick={this.loadImage}>
+                    Load Image
                 </Button>
             </div>
         );
