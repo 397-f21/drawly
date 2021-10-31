2068230b69dd662f0a2cb194afed61ad3b734d2c
mitchelllai <mitchelllaimain@gmail.com>
2021-10-30 19:24:43 -0500

    passing tab test and added test-ids to divs

--- src/test/Tab.test.js
+++ src/test/Tab.test.js
@@ -1,6 +1,22 @@
-import {act, render, fireEvent} from "@testing-library/react"
+import {render, fireEvent, screen, waitFor} from "@testing-library/react"
 import App from '../App'
 
-test('tab switches modes', () => {
-    render(<App/>)
+const mockEnqueue = jest.fn();
+jest.mock('notistack', () => ({
+    ...jest.requireActual('notistack'),
+    useSnackbar: () => {
+        return {
+            enqueueSnackbar: mockEnqueue
+        };
+    }
+}));
+
+test('tab switches mode', async () => {
+    await waitFor(() => render(<App/>));
+    const drawTab = screen.getByTestId('draw-tab');
+    const calTab = screen.getByTestId('cal-tab');
+    await waitFor(() => fireEvent.click(calTab));
+    expect(screen.getByTestId('cal')).toBeTruthy();
+    await waitFor(() => fireEvent.click(drawTab));
+    expect(screen.getByTestId('canvas')).toBeTruthy();
 })
