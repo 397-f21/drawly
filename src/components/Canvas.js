90946dee24048e92cdb8cd95de4de10ee38106b8
Katie Li <katie01px2019@sasstudent.org>
2021-11-02 00:03:39 -0500

    syling canvas tools & cal popup

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -7,11 +7,16 @@ import { signInWithGoogle, signOut, useUserState } from '../utilities/firebase.j
 import { useSnackbar } from 'notistack';
 import ToggleButton from 'react-bootstrap/ToggleButton'
 import {isMorning, isAfternoon} from '../App.js';
+import '../Canvas.svg';
 
 const today = new Date();
 
 const getButtonStyling = (today) => {
     return isMorning(today) ? 'morning-button-styling' : isAfternoon(today) ? 'afternoon-button-styling' : 'evening-button-styling';
+}
+
+const getClearButtonStyling = (today) => {
+    return isMorning(today) ? 'morning-clear-button-styling' : isAfternoon(today) ? 'afternoon-clear-button-styling' : 'evening-clear-button-styling';
 }
 
 const getSigninButtonStyling = (today) => {
@@ -43,15 +48,17 @@ const EraseCheckedButton = React.forwardRef((props, ref) => {
     const [checked, setChecked] = useState(true);
     return (
         <ToggleButton
-            className="mb-2"
+            className="tool-circle"
             id="toggle-check"
             type="checkbox"
-            variant="outline-primary"
+            variant="tool-circle"
             checked={checked}
             value="0"
             onChange={(e) => { setChecked(e.currentTarget.checked); ref.current.eraseMode(checked) }}
         >
-            Eraser
+            <svg width="30" height="30" viewBox="0 0 29 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M26.9221 25.0519H16.5779L27.8864 13.7434C28.4189 13.2109 28.4189 12.3475 27.8864 11.8149L16.9773 0.905848C16.4448 0.373303 15.5814 0.373303 15.0488 0.905848L-1.31484 17.2694C-1.57066 17.5251 -1.71429 17.8719 -1.71429 18.2337C-1.71429 18.5954 -1.57066 18.9422 -1.31484 19.1979L6.86699 27.3797C7.12271 27.6355 7.46953 27.7791 7.83117 27.7791L13.2857 27.7792C13.2863 27.7792 13.2867 27.7791 13.2873 27.7791L26.9221 27.7792C27.6751 27.7792 28.2857 27.1687 28.2857 26.4156C28.2857 25.6625 27.6752 25.0519 26.9221 25.0519ZM16.013 3.79857L24.9936 12.7792L17.1039 20.6689L8.12326 11.6883L16.013 3.79857Z" fill="white"/>
+            </svg>
         </ToggleButton>
     )
 
@@ -59,15 +66,17 @@ const EraseCheckedButton = React.forwardRef((props, ref) => {
 
 const ClearButton = React.forwardRef((props, ref) => (
     <Button
-    data-testid='clear-button' onClick={() => ref.current.clearCanvas()} className={getButtonStyling(today)}>
+    data-testid='clear-button' onClick={() => ref.current.clearCanvas()} className={getClearButtonStyling(today)}>
         Clear Canvas
     </Button>
 ));
 
 const UnDo = React.forwardRef((props, ref) => (
-    <Button onClick={() => ref.current.undo()} className='undo-button-styling'>
-        undo
-    </Button>
+    <div onClick={() => ref.current.undo()} className="tool-circle">
+        <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18.2292 11.6667C14.3646 11.6667 10.8646 13.1104 8.16666 15.4583L2.91666 10.2083V23.3333H16.0417L10.7625 18.0542C12.7896 16.3625 15.3708 15.3125 18.2292 15.3125C23.3917 15.3125 27.7812 18.6812 29.3125 23.3333L32.7687 22.1958C30.7417 16.0854 25.0104 11.6667 18.2292 11.6667Z" fill="white"/>
+        </svg>
+    </div>
 ));
 
 const Canvas = React.forwardRef((props, ref) => {
@@ -124,30 +133,41 @@ const Canvas = React.forwardRef((props, ref) => {
                     width={300} // make dynamic
                 />
             </div>
+            <div className='tools-grid'>
+                <div style={{ gridArea: 'brush'}}>
+                    <input
+                    type="color"
+                    className="tool-circle"
+                    data-testid='stroke-color-input'
+                    value={strokeColor}
+                    onChange={(e) => {
+                        setStrokeColor(e.target.value);
+                    }}></input>
+                </div>
+                <div style={{ gridArea: 'background'}}>
+                    <input
+                    type="color"
+                    className="tool-circle"
+                    data-testid='canvas-color-input'
+                    value={canvasColor}
+                    onChange={(e) => {
+                        setCanvasColor(e.target.value);
+                    }}>
+                    </input>
+                </div>
+                <div style={{ gridArea: 'eraser'}}>
+                    <EraseCheckedButton ref={canvas} />
+                </div>
+                <div style={{ gridArea: 'undo'}}>
+                    <UnDo ref={canvas} />
+                </div>
+            </div>
+        
+            <ClearButton ref={canvas} />
+            
             {user ? <Button className={getButtonStyling(today)} onClick={saveImage}>
                 Check in
             </Button> : null}
-            <UnDo ref={canvas} />
-            <EraseCheckedButton ref={canvas} />
-            <ClearButton ref={canvas} />
-            <input
-                type="color"
-                className="form-control form-control-color"
-                data-testid='stroke-color-input'
-                value={strokeColor}
-                onChange={(e) => {
-                    setStrokeColor(e.target.value);
-                }}
-            ></input>
-            <input
-                type="color"
-                className="form-control form-control-color"
-                data-testid='canvas-color-input'
-                value={canvasColor}
-                onChange={(e) => {
-                    setCanvasColor(e.target.value);
-                }}
-            ></input>
             {user ? <SignOutButton/> : <SignInButton className = {getSigninButtonStyling(today)}/>}
         </div>
     );
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -20,10 +20,16 @@ const Icon = ({ key, svg, loc }) => {
     return (
         <div className='calendar-entry-wrapper' style={{ gridArea: loc }}>
             <Image key={key} className='calendar-entry' onClick={openModal} src={svg} roundedCircle />
-            <ReactModal isOpen={modalVisible} onRequestClose={closeModal}>
-                <div className="modal-text">Filter by Line</div>
-                <Image key={key} onClick={openModal} src={svg} roundedCircle />
-                <button className="btn btn-primary" onClick={closeModal}>Close</button>
+            <ReactModal isOpen={modalVisible} onRequestClose={closeModal} className="modal-styling">
+                <div onClick={closeModal}>
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.8333 5.34167L14.6583 4.16667L9.99996 8.82501L5.34163 4.16667L4.16663 5.34167L8.82496 10L4.16663 14.6583L5.34163 15.8333L9.99996 11.175L14.6583 15.8333L15.8333 14.6583L11.175 10L15.8333 5.34167Z" fill="#DCDCDC"/>
+                        </svg>
+                </div>
+                <div className="modal-text">Your drawing on</div>
+                <div className="modal-text-date">Date</div>
+                <Image key={key} className='modal-face' onClick={openModal} src={svg} roundedCircle />
+                
             </ReactModal>
         </div>
     )
