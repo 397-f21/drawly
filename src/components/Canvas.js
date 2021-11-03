fd31f2fdb1a0a3675e1330fea9ce35d5f0ae0b00
Katie Li <katie01px2019@sasstudent.org>
2021-11-02 21:15:00 -0500

    styling changes

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -43,26 +43,7 @@ const SignOutButton = () => (
     </Button>
 );
 
-const EraseCheckedButton = React.forwardRef((props, ref) => {
 
-    const [checked, setChecked] = useState(true);
-    return (
-        <ToggleButton
-            className="tool-circle"
-            id="toggle-check"
-            type="checkbox"
-            variant="tool-circle"
-            checked={checked}
-            value="0"
-            onChange={(e) => { setChecked(e.currentTarget.checked); ref.current.eraseMode(checked) }}
-        >
-            <svg width="30" height="30" viewBox="0 0 29 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M26.9221 25.0519H16.5779L27.8864 13.7434C28.4189 13.2109 28.4189 12.3475 27.8864 11.8149L16.9773 0.905848C16.4448 0.373303 15.5814 0.373303 15.0488 0.905848L-1.31484 17.2694C-1.57066 17.5251 -1.71429 17.8719 -1.71429 18.2337C-1.71429 18.5954 -1.57066 18.9422 -1.31484 19.1979L6.86699 27.3797C7.12271 27.6355 7.46953 27.7791 7.83117 27.7791L13.2857 27.7792C13.2863 27.7792 13.2867 27.7791 13.2873 27.7791L26.9221 27.7792C27.6751 27.7792 28.2857 27.1687 28.2857 26.4156C28.2857 25.6625 27.6752 25.0519 26.9221 25.0519ZM16.013 3.79857L24.9936 12.7792L17.1039 20.6689L8.12326 11.6883L16.013 3.79857Z" fill="white"/>
-            </svg>
-        </ToggleButton>
-    )
-
-})
 
 const ClearButton = React.forwardRef((props, ref) => (
     <Button
@@ -134,7 +115,7 @@ const Canvas = React.forwardRef((props, ref) => {
                 />
             </div>
             <div className='tools-grid'>
-                <div style={{ gridArea: 'brush'}}>
+                <div>
                     <div className='tool-circle'
                     style={{backgroundColor: strokeColor}}>
                         <input
@@ -148,7 +129,7 @@ const Canvas = React.forwardRef((props, ref) => {
                         }}></input>
                     </div>
                 </div>
-                <div style={{ gridArea: 'background'}}>
+                <div>
                     <div className='tool-circle'
                     style={{backgroundColor: canvasColor}}>
                         <input
@@ -163,10 +144,7 @@ const Canvas = React.forwardRef((props, ref) => {
                         </input>
                     </div>
                 </div>
-                <div style={{ gridArea: 'eraser'}}>
-                    <EraseCheckedButton ref={canvas} />
-                </div>
-                <div style={{ gridArea: 'undo'}}>
+                <div>
                     <UnDo ref={canvas} />
                 </div>
             </div>
