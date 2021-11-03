ffbaceac8581c90c4c57360ac57fc9f74d5fc033
mitchelllai <mitchelllaimain@gmail.com>
2021-11-02 21:47:29 -0500

    centered modal and date

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -4,7 +4,18 @@ import Image from 'react-bootstrap/Image'
 
 import ReactModal from 'react-modal';
 
-const Icon = ({ key, svg, loc }) => {
+const DAY_MAP = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const MONTH_MAP = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+const customStyles = {
+    overlay:{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+}
+
+const Icon = ({ key, date_string, svg, loc }) => {
     const [modalVisible, setModalVisible] = useState(false);
 
     ReactModal.setAppElement('#root');
@@ -17,19 +28,28 @@ const Icon = ({ key, svg, loc }) => {
         setModalVisible(false);
     }
 
+    const getDate = (date_string) => {
+        console.log(date_string)
+        const date = new Date(date_string);
+        console.log(date)
+        const month = MONTH_MAP[date.getMonth()];
+        const day = DAY_MAP[date.getDay()];
+        const this_date = date.getDate();
+        return day + ', ' + month + ' ' + this_date;
+    }
+
     return (
         <div className='calendar-entry-wrapper' style={{ gridArea: loc }}>
             <Image key={key} className='calendar-entry' onClick={openModal} src={svg} roundedCircle />
-            <ReactModal isOpen={modalVisible} onRequestClose={closeModal} className="modal-styling">
+            <ReactModal isOpen={modalVisible} onRequestClose={closeModal} className="modal-styling" style={customStyles}>
                 <div onClick={closeModal}>
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15.8333 5.34167L14.6583 4.16667L9.99996 8.82501L5.34163 4.16667L4.16663 5.34167L8.82496 10L4.16663 14.6583L5.34163 15.8333L9.99996 11.175L14.6583 15.8333L15.8333 14.6583L11.175 10L15.8333 5.34167Z" fill="#DCDCDC"/>
                         </svg>
                 </div>
                 <div className="modal-text">Your drawing on</div>
-                <div className="modal-text-date">Date</div>
+                <div className="modal-text-date">{getDate(date_string)}</div>
                 <Image key={key} className='modal-face' onClick={openModal} src={svg} roundedCircle />
-                
             </ReactModal>
         </div>
     )
