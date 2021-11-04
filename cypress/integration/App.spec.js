0aa05e26c6069fd0fed6deb8b93bafc095cf43b1
caedonhsieh <caedonhsieh@gmail.com>
2021-11-04 16:42:49 -0500

    interaction test and raise tab

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -7,4 +7,10 @@ describe ('Test App', () => {
       cy.visit ('/');
       cy.get('[data-cy=canvas-layout]').should('contain', (new Date()).getDate());
     });
+
+    it ('Clicking draw tab shows calendar', () => {
+      cy.visit ('/');
+      cy.get('[data-cy=draw-tab]').click();
+      cy.get('[data-cy=cal]').should('be.visible');
+    });
 });
